1d2de0c10b1850ff8348c3744616b0ffd20c07f6
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-01 21:07:16 +0200

    uncomment validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,15 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
-  # validates :title,
-  #           presence: true,
-  #           length: {in: 3..25}
-  #
-  # validates :description,
-  #           length: {maximum: 1500}
-  # validates :image_url,
-  #           presence: true
-  # validates :price,
-  #           presence: true,
-  #           numericality: { greater_than: 0, less_than: 100 }
+  validates :title,
+            presence: true,
+            length: {in: 3..25}
+
+  validates :description,
+            length: {maximum: 1500}
+  validates :image_url,
+            presence: true
+  validates :price,
+            presence: true,
+            numericality: { greater_than: 0, less_than: 100 }
 end
