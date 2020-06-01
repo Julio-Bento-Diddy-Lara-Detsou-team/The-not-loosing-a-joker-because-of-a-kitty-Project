6b0a2d9c5b7aa38b8ed313cc5ba87bd25c2b1edc
Tribouillier Julien <62880018+Popikadir@users.noreply.github.com>
2020-06-01 21:34:03 +0200

    [DB] Add seeding to populate fake data to the database
    * add seed Items and change description in database
    
    * add User seed
    
    * add Order seed and change stipe in stripe_customer_id
    
    * add user_id in Order's Seed
    
    * add functionnal seed
    
    * uncomment validates
    
    * add .idea .floo .flooignore .DSstore to .gitignore
    
    * change text un Order seed
    
    * fix image_url seed

--- db/migrate/20200601113153_create_items.rb
+++ db/migrate/20200601113153_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :title
-      t.text :descritpion
+      t.text :description
       t.string :image_url
       t.float :price
 
