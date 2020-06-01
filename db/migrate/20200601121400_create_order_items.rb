40012dacc1e6a1a741506d0788e54b01b31bce9c
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-01 14:25:31 +0200

    add index true to belongs_to

--- db/migrate/20200601121400_create_order_items.rb
+++ db/migrate/20200601121400_create_order_items.rb
@@ -3,8 +3,8 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
     create_table :order_items do |t|
       t.integer :quantity
       t.float :total_price
-      t.belongs_to :order
-      t.belongs_to :item
+      t.belongs_to :order, index: true
+      t.belongs_to :item, index: true
       t.timestamps
     end
   end
