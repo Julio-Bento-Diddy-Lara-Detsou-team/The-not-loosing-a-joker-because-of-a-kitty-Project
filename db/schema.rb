1effce44dcf1673d202ae584fb78cfc719bf9bdb
Laramarey <l.schutz@tbs-education.org>
2020-06-01 16:05:15 +0200

    [Front] Ajout des dossiers bootstrap.css, bootstrap.js et jquery.js dans le dossier assets, collage de la Mise En Forme du thème choisi dans la homepage en index.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_114820) do
+ActiveRecord::Schema.define(version: 2020_06_01_121400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "descritpion"
+    t.string "image_url"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.float "total_price"
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "stipe_customer_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "shopping_sessions", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_shopping_sessions_on_cart_id"
+    t.index ["item_id"], name: "index_shopping_sessions_on_item_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
