959e40d77fd56cdbe1892834ac0d223b658d3fdc
didy400 <59975329+didy400@users.noreply.github.com>
2020-06-02 13:36:06 +0200

    Cleaning-up index.html.erb + css scss des éléments liés
    * add item controller and show dynamic data in index
    
    * [UI&UX] Fix cards design and css links
    
    * [Floo] Delete .floo
    
    Co-authored-by: PoPiKaDiR <julien.tribouillier@gmail.com>
    Co-authored-by: Bento <oobento01oo@gmail.com>
    Co-authored-by: Detsou <62930936+Detsou@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,18 @@ orders = []
   number_photo = rand(300..399)
 
   items << Item.create!(
-      title: Faker::Creature::Cat.name,
-      description: Faker::Lorem.paragraph,
-      image_url: "https://placekitten.com/200/#{number_photo}",
-      price: rand(1..5))
+    title: Faker::Creature::Cat.name,
+    description: Faker::Lorem.paragraph,
+    image_url: "https://placekitten.com/200/#{number_photo}",
+    price: rand(1..5))
   x += 1
   puts "#{x}/20 items created"
 end
 
 10.times do |x|
   users << User.create!(
-      email: Faker::Name.first_name + "@yopmail.com" ,
-      password: "1234567"
+    email: Faker::Name.first_name + "@yopmail.com" ,
+    password: "1234567"
   )
   x += 1
   puts "#{x}/10 users created"
@@ -30,8 +30,8 @@ end
 
 30.times do |x|
   orders << Order.create!(
-      user: users.sample,
-      stripe_customer_id: Faker::Code.ean
+    user: users.sample,
+    stripe_customer_id: Faker::Code.ean
   )
   x += 1
   puts "#{x}/30 Orders created"
@@ -39,10 +39,10 @@ end
 
 30.times do |x|
   OrderItem.create!(
-      order: orders.sample,
-      item: items.sample,
-      quantity: rand(1...5),
-      total_price:  rand(5...200)
+    order: orders.sample,
+    item: items.sample,
+    quantity: rand(1...5),
+    total_price: rand(5...200)
   )
   x += 1
   puts "#{x}/30 OrderItem created"
