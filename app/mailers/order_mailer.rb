433c5066a5205cbaf2fc14b4ac035f8844bdf6eb
Detsou <detsouvan@hotmail.com>
2020-06-04 17:47:21 +0200

    [Back]Order Mailer method send email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,9 @@
 class OrderMailer < ApplicationMailer
   default from: 'moufaca755@gmail.com'
 
-  def order_email(user)
-    @user = user
+  def order_email(order)
+    @order = order
+    @user = @order.user
     @cart = @user.cart
     @url = 'https://thp-keety-sweety.herokuapp.com/'
     mail(to: @user.email, subject: 'Votre commande Kitty Sweety !')
