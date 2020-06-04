b99ad2fb41969f8dbf07b26b60538ec2f91028c4
PoPiKaDiR <julien.tribouillier@gmail.com>
2020-06-04 12:06:33 +0200

    [BDD-Front]add categories to items' index and fix price view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_084842) do
+ActiveRecord::Schema.define(version: 2020_06_04_095643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 2020_06_04_084842) do
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.integer "price"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
