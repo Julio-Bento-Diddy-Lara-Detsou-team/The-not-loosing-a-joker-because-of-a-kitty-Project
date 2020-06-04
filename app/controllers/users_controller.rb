419e0d63df625f31673a614d1fbcd28d10671182
Laramarey <l.schutz@tbs-education.org>
2020-06-04 20:48:06 +0200

    [Front/Back]fixed views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def show
-    # @user = current_user
+    @user = current_user
   end
 
   def edit
-    # @user = current_user
+    @user = current_user
   end
 
   def update
-    # @user = current_user
+    @user = current_user
   end
 end
